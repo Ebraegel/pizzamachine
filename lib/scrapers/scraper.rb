54cde9ee4472c4872a53fb11bfe13cf9adbaec19
Erik Braegelmann <erik.braegelmann@govdelivery.com>
2018-04-06 12:35:11 -0500

    oops, rip out some more stuff

--- lib/scrapers/scraper.rb
+++ lib/scrapers/scraper.rb
@@ -4,7 +4,6 @@ require 'pry-byebug'
 
 require_relative './luce.rb'
 require_relative './parkway.rb'
-require_relative './reds_savoy.rb'
 require_relative './davannis.rb'
 require_relative './carbones.rb'
 
